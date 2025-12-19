90a7db65fe94e66f212a2286350732f21727b54a
amirkhan <akhan@technologyrivers.com>
2025-12-19 23:39:17 +0300

    Configure base URL for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: "/React_Story_Book/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
